7422e8702fe5d3a18d80334a4cc3004dba51993f
Vail Algatt <vail.algatt@gmail.com>
2017-08-22 18:09:19 -0600

    file structure set up and ready to get started! :+1:

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
