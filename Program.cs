82311936edaabd4a4f76140083d09448234522a5
Vail Algatt <vail.algatt@gmail.com>
2017-08-24 22:17:05 -0600

    installed nuget package manager!

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
 namespace Grades
 {
@@ -10,6 +11,8 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            SpeechSynthesizer
+
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
